ef7aa689125b407555c4ba650cdefdcf9c14e00f
Jacky <jackychen924@gmail.com>
2024-12-08 20:06:43 +0800

    feat: add create event page to menu

--- frontend/app/GlobalLayout.tsx
+++ frontend/app/GlobalLayout.tsx
@@ -1,4 +1,6 @@
-import { Layout } from "antd";
+import { HomeOutlined, PlusOutlined } from "@ant-design/icons";
+import { ConfigProvider, Layout, Menu, MenuProps } from "antd";
+import { useState } from "react";
 import { Link } from "react-router";
 
 const { Header, Content } = Layout;
@@ -8,16 +10,62 @@ export default function GlobalLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <Layout className="w-full h-full">
-      <Header className="sticky top-0 z-10 w-full flex items-center">
+  const [selectedMenuKey, setSelectedMenuKey] = useState<string>("homeLink");
+
+  const menuItems: MenuProps["items"] = [
+    {
+      key: "homeLink",
+      icon: (
         <Link to="/">
-          <h1 className="text-3xl text-white">Event Ticketing Platform</h1>
+          <HomeOutlined />
         </Link>
-      </Header>
-      <Content className="py-2 bg-sol-pale">
-        <div className="p-6 min-h-96">{children}</div>
-      </Content>
-    </Layout>
+      ),
+      label: "Home",
+    },
+    {
+      key: "createLink",
+      icon: (
+        <Link to="/create">
+          <PlusOutlined />
+        </Link>
+      ),
+      label: "Create Event",
+    },
+  ];
+
+  return (
+    <ConfigProvider
+      theme={{
+        components: {
+          Menu: {
+            darkItemSelectedBg: "#005d8d",
+            activeBarHeight: 5,
+          },
+        },
+      }}
+    >
+      <Layout className="w-full h-full">
+        <Header className="sticky top-0 z-10 w-full flex items-center">
+          <Link
+            to="/"
+            className="mr-5"
+            onClick={() => setSelectedMenuKey("homeLink")}
+          >
+            <h1 className="text-3xl text-white">Event Ticketing Platform</h1>
+          </Link>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            items={menuItems}
+            style={{ flex: 1, minWidth: 0 }}
+            selectedKeys={[selectedMenuKey]}
+            onClick={(e) => setSelectedMenuKey(e.key)}
+          />
+        </Header>
+        <Content className="py-2 bg-sol-pale">
+          <div className="p-6 min-h-96">{children}</div>
+        </Content>
+      </Layout>
+    </ConfigProvider>
   );
 }
--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("event/:eventId", "routes/EventDetailPage.tsx")
+    route("event/:eventId", "routes/EventDetailPage.tsx"),
+    route("create", "routes/CreateEventPage.tsx")
 ] satisfies RouteConfig;
